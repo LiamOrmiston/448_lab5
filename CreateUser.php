ea4574ab296914b27aaa682b362c5127a5ab71b1
LiamOrmiston <ljormiston@ku.edu>
2017-10-22 14:04:37 -0500

    syntax and layout

--- CreateUser.php
+++ CreateUser.php
@@ -1,31 +1,33 @@
-<?php
-$mysqli = new mysqli('mysql.eecs.ku.edu', 'lormiston', 'P@$$word123', 'lormiston');
+<html>
+  <body>
+  <?php
+  $mysqli = new mysqli('mysql.eecs.ku.edu', 'lormiston', 'P@$$word123', 'lormiston');
 
-// connection test
-if ($mysqli->connect_errno) {
-    printf("Connect failed: %s\n", $mysqli->connect_error);
-    exit();
-}
+  // connection test
+  if ($mysqli->connect_errno) {
+      printf("Connect failed: %s\n", $mysqli->connect_error);
+      exit();
+  }
 
-$username = $_POST["username"];
-echo '<body>';
-echo '<div>';
-echo '<h2>';
-if ($username == ''){
-  echo 'Username cannot be blank';
-}
-else {
-  $query = "INSERT INTO Users (user_id) VALUES ('$user_id');";
-  if($result = mspli.query($username)) {
-    echo "<p>Hello " . $username . "!</p>"
+  $username = $_POST['username'];
+  echo '<div>';
+  echo '<h2>';
+  if ($username == ''){
+    echo 'Username cannot be blank';
   }
-  else{
-    echo "$mysqli->error"
+  else {
+    $query = "INSERT INTO Users (user_id) VALUES ('$user_id');";
+    if($result = mspli.query($username)) {
+      echo "<p>Hello " . $username . "!</p>"
+    }
+    else{
+      echo "$mysqli->error"
+    }
+    $result->free();
   }
-  $result->free();
-}
-echo '</h2>';
-echo '</div>';
-echo '</body>';
-$mysqli->close();
-?>
+  echo '</h2>';
+  echo '</div>';
+  $mysqli->close();
+  ?>
+  </body>
+</html>
