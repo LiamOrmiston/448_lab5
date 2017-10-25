3e88e341caf990fca412bfbf32d4b13f4722e163
LiamOrmiston <ljormiston@ku.edu>
2017-10-24 23:47:04 -0500

    generate table for users

--- ViewUsers.php
+++ ViewUsers.php
@@ -9,7 +9,21 @@
     }
 
     echo '<div>';
-    
+    $user_query = "SELECT user_id FROM Users";
+    $result = $mysqli->query($user_query);
+    if(mysqli_num_rows($result) > 0){
+    	echo '<h2>Users</h2>';
+    	echo '<table>';
+    	echo '<tr><th>user_id</th></tr>';
+    	while ($user = $result->fetch_assoc()){
+    		echo "<tr><td>" . $user['user_id'] . "</td></tr>";
+    	}
+    	echo '</table>';
+    }
+    else{
+    	echo '<h2>No users exist</h2>';
+    }
+    $result->free();
     echo '</div>';
     $mysqli->close();
     ?>
