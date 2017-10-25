2876ac23a830f89d5322c4b8af54fd358f3b7769
LiamOrmiston <ljormiston@ku.edu>
2017-10-25 00:31:47 -0500

    getting things set up

---
+++ ViewUserPosts.php
@@ -0,0 +1,31 @@
+<html>
+  <body>
+    <?php
+    $mysqli = new mysqli('mysql.eecs.ku.edu', 'lormiston', 'P@$$word123', 'lormiston');
+    // connection test
+    if ($mysqli->connect_errno) {
+        printf("Connect failed: %s\n", $mysqli->connect_error);
+        exit();
+    }
+
+    echo '<div>';
+    $query_user = "SELECT user_id FROM Users";
+    $result = $mysqli->query($query_user);
+    if(mysqli_num_rows($result) > 0){
+    	echo '<h2>Users</h2>';
+    	echo '<table>';
+    	echo '<tr><th>user_id</th></tr>';
+    	while ($user = $result->fetch_assoc()){
+    		echo "<tr><td>" . $user['user_id'] . "</td></tr>";
+    	}
+    	echo '</table>';
+    }
+    else{
+    	echo '<h2>No users exist</h2>';
+    }
+    $result->free();
+    echo '</div>';
+    $mysqli->close();
+    ?>
+  </body>
+</html>
