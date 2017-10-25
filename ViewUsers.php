7114b45290da78e95c9ba91b3e188513d0a177f9
LiamOrmiston <ljormiston@ku.edu>
2017-10-24 23:38:55 -0500

    setting up

---
+++ ViewUsers.php
@@ -0,0 +1,17 @@
+<html>
+  <body>
+    <?php
+    $mysqli = new mysqli('mysql.eecs.ku.edu', 'lormiston', 'P@$$word123', 'lormiston');
+    // connection test
+    if ($mysqli->connect_errno) {
+        printf("Connect failed: %s\n", $mysqli->connect_error);
+        exit();
+    }
+
+    echo '<div>';
+    
+    echo '</div>';
+    $mysqli->close();
+    ?>
+  </body>
+</html>
