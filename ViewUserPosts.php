4e8ab82cf4e3e96a3fe4235c720cde85330f673a
LiamOrmiston <ljormiston@ku.edu>
2017-10-25 01:27:24 -0500

    more testing

--- ViewUserPosts.php
+++ ViewUserPosts.php
@@ -9,6 +9,7 @@
     }
 
     echo '<div>';
+    echo "<h2>Select what user's posts you'd like to see</h2>";
     echo '<form action="ViewUserPosts.php" method="POST">';
       $query_user = "SELECT * FROM Users";
       $result = $mysqli->query($query_user);
@@ -31,17 +32,19 @@
     $username = $_POST['username'];
     $query_post = "SELECT post_id, content, author_id FROM Posts WHERE author_id='$username'";
     $result = $mysqli->query($query_post);
-    if(mysqli_num_rows($result) > 0){
-    	echo "<h2>$username's posts:</h2>";
-    	echo '<table>';
-    	echo '<tr><th>post_id</th><th>content</th></tr>';
-    	while ($post = $result->fetch_assoc()){
-    		echo "<tr><td>" . $post['post_id'] . "</td><td>" . $post['content'] . "</td></tr>";
-    	}
-    	echo '</table>';
-    }
-    else{
-    	echo "<h2>$username has not made any posts yet!</h2>";
+    if ($username != ''){
+      if(mysqli_num_rows($result) > 0){
+      	echo "<h2>$username's posts:</h2>";
+      	echo '<table>';
+      	echo '<tr><th>post_id</th><th>content</th></tr>';
+      	while ($post = $result->fetch_assoc()){
+      		echo "<tr><td>" . $post['post_id'] . "</td><td>" . $post['content'] . "</td></tr>";
+      	}
+      	echo '</table>';
+      }
+      else{
+      	echo "<h2>$username has not made any posts yet!</h2>";
+      }
     }
     $result->free();
     echo '</div>';
